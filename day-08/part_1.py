364272aab4d3946bea97521e0f3db0c5cc682588
Siddharth Sahani <sidhucroedde@gmail.com>
2023-12-08 21:28:36 +0530

    Added code for day-08

---
+++ day-08/part_1.py
@@ -0,0 +1,25 @@
+
+file = open("day-08/inputs/part_1.txt", 'r')
+file_contents = file.read()
+
+
+seq, nodes = file_contents.split('\n\n')
+lr_nodes = {}
+N = len(nodes)
+
+
+for id, line in enumerate(nodes.splitlines()):
+    node, lr = line.split(' = ')
+    l, r = lr[1:-1].split(', ')
+    lr_nodes[node] =  [l, r]
+
+
+num_steps = 0
+
+cur_node = 'AAA'
+while cur_node != 'ZZZ':
+    inst = seq[num_steps % len(seq)]
+    cur_node = lr_nodes[cur_node][0 if inst == 'L' else 1]
+    num_steps += 1
+
+print(num_steps)
---
+++ day-08/part_2.py
@@ -0,0 +1,31 @@
+
+from math import lcm
+
+
+file = open("day-08/inputs/part_2.txt", 'r')
+file_contents = file.read()
+
+
+seq, nodes = file_contents.split('\n\n')
+lr_nodes = {}
+N = len(nodes)
+
+
+for id, line in enumerate(nodes.splitlines()):
+    node, lr = line.split(' = ')
+    l, r = lr[1:-1].split(', ')
+    lr_nodes[node] =  [l, r]
+
+
+num_steps = []
+cur_nodes = [node for node in lr_nodes if node[2] == 'A']
+
+for cur_node in cur_nodes:
+    num_step = 0
+    while cur_node[2] != 'Z':
+        inst = seq[num_step % len(seq)]
+        cur_node = lr_nodes[cur_node][0 if inst == 'L' else 1]
+        num_step += 1
+    num_steps.append(num_step)
+
+print(lcm(*num_steps))
